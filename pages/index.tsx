67dfc4c1e1b9b9fd1bbac6a20077a3f378f404ae
markoz <markdrew53@gmail.com>
2020-05-28 12:17:08 -0500

    Added description

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
+import Layout from '@components/Layout';
+
 const Index = () => {
   return (
-    <div>
-      <h1>hola</h1>
-    </div>
+    <Layout select="0">
+      <div className="explanation">
+        The objective of this application is to show the benefits of{' '}
+        <b>Static Site Generation</b> (SSG). Select any character that is
+        located in the sider : )
+      </div>
+      <style jsx>{`
+        .explanation {
+          font-size: 1rem;
+          width: 35rem;
+          max-width: 100vw;
+          padding: 0 2rem;
+          margin: 0.5rem auto;
+        }
+      `}</style>
+    </Layout>
   );
 };
 
